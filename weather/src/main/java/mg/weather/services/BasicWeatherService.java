d54f7714af4ca7e924520dddafd8271b91c2f93f
mgelios <master_gelios@tut.by>
2018-11-01 15:46:13 +0300

    updated db interaction for weather module

--- weather/src/main/java/mg/weather/services/BasicWeatherService.java
+++ weather/src/main/java/mg/weather/services/BasicWeatherService.java
@@ -39,48 +39,50 @@ public class BasicWeatherService implements WeatherService {
     @Autowired
     CurrentWeatherToCurrentWeatherDBEntity currentWeatherToCurrentWeatherDBEntity;
 
-
     @Override
-    public CurrentWeather getDefaultWeatherInfo() {
-        CurrentWeather weather = new CurrentWeather();
+    public CurrentWeather getDefaultWeatherInfo(){
+        return getWeatherInfo(weatherConfiguration.getDefaultCity());
+    }
+
+    public CurrentWeather getWeatherInfo(String cityName) {
         CurrentWeatherDBEntity dbEntity = null;
-        if (currentWeatherRepository.findByCityName("minsk").isPresent()) {
-            dbEntity = currentWeatherRepository.findByCityName("minsk").get();
+        if (currentWeatherRepository.findByCityName(cityName).isPresent()) {
+            dbEntity = currentWeatherRepository.findByCityName(cityName).get();
             int dbCurrentWeatherHours = dbEntity.getTime().toLocalDateTime().getHour() / 2;
             int currentTimeHours = LocalDateTime.now().getHour() / 2;
             if (currentTimeHours != dbCurrentWeatherHours) {
-
-            } else {
-                return currentWeatherDBEntityToCurrentWeather.convert(dbEntity);
+                currentWeatherRepository.delete(dbEntity);
+                dbEntity = writeCurrentWeatherToDB(cityName);
             }
         } else {
-            JSONObject json = jsonConsumer.getJson(weatherUrlBuilder.buildCurrentDefaultWeatherUrl());
-            dbEntity = new CurrentWeatherDBEntity();
-            dbEntity.setLatitude(Double.valueOf(((JSONObject)json.get("coord")).get("lat").toString()));
-            dbEntity.setLongitude(Double.valueOf(((JSONObject)json.get("coord")).get("lon").toString()));
-            dbEntity.setDescription(((JSONObject)((JSONArray) json.get("weather")).get(0)).get("description").toString());
-            dbEntity.setMainInfo(((JSONObject)((JSONArray) json.get("weather")).get(0)).get("main").toString());
-            dbEntity.setIcon(((JSONObject)((JSONArray) json.get("weather")).get(0)).get("icon").toString());
-            dbEntity.setTemperature(Double.valueOf(((JSONObject)json.get("main")).get("temp").toString()));
-            dbEntity.setPressure(Double.valueOf(((JSONObject)json.get("main")).get("pressure").toString()));
-            dbEntity.setHumidity(Double.valueOf(((JSONObject)json.get("main")).get("humidity").toString()));
-            dbEntity.setMinimalTemperature(Double.valueOf(((JSONObject)json.get("main")).get("temp_min").toString()));
-            dbEntity.setMaximumTemperature(Double.valueOf(((JSONObject)json.get("main")).get("temp_max").toString()));
-            dbEntity.setVisibility(Double.valueOf(json.get("visibility").toString()));
-            dbEntity.setWindSpeed(Double.valueOf(((JSONObject)json.get("wind")).get("speed").toString()));
-            dbEntity.setWindDegree(Double.valueOf(((JSONObject)json.get("wind")).get("deg").toString()));
-            dbEntity.setTime(Timestamp.from(Instant.ofEpochSecond(Long.valueOf(json.get("dt").toString()))));
-            dbEntity.setSunrise(Timestamp.from(Instant.ofEpochSecond(Long.valueOf(((JSONObject)json.get("sys")).get("sunrise").toString()))));
-            dbEntity.setSunset(Timestamp.from(Instant.ofEpochSecond(Long.valueOf(((JSONObject)json.get("sys")).get("sunset").toString()))));
-            dbEntity.setCityName(json.get("name").toString().toLowerCase());
+            dbEntity = writeCurrentWeatherToDB(cityName);
             currentWeatherRepository.save(dbEntity);
             dbEntity = currentWeatherRepository.findByCityName("minsk").get();
-            return currentWeatherDBEntityToCurrentWeather.convert(dbEntity);
         }
-        weather.setCityName("Minsk");
-        weather.setId(2L);
-        weather.setHumidity(200);
-        return weather;
+        return currentWeatherDBEntityToCurrentWeather.convert(dbEntity);
+    }
+
+    private CurrentWeatherDBEntity writeCurrentWeatherToDB(String cityName){
+        JSONObject json = jsonConsumer.getJson(weatherUrlBuilder.buildCurrentWeatherUrl(cityName));
+        CurrentWeatherDBEntity dbEntity = new CurrentWeatherDBEntity();
+        dbEntity.setLatitude(Double.valueOf(((JSONObject)json.get("coord")).get("lat").toString()));
+        dbEntity.setLongitude(Double.valueOf(((JSONObject)json.get("coord")).get("lon").toString()));
+        dbEntity.setDescription(((JSONObject)((JSONArray) json.get("weather")).get(0)).get("description").toString());
+        dbEntity.setMainInfo(((JSONObject)((JSONArray) json.get("weather")).get(0)).get("main").toString());
+        dbEntity.setIcon(((JSONObject)((JSONArray) json.get("weather")).get(0)).get("icon").toString());
+        dbEntity.setTemperature(Double.valueOf(((JSONObject)json.get("main")).get("temp").toString()));
+        dbEntity.setPressure(Double.valueOf(((JSONObject)json.get("main")).get("pressure").toString()));
+        dbEntity.setHumidity(Double.valueOf(((JSONObject)json.get("main")).get("humidity").toString()));
+        dbEntity.setMinimalTemperature(Double.valueOf(((JSONObject)json.get("main")).get("temp_min").toString()));
+        dbEntity.setMaximumTemperature(Double.valueOf(((JSONObject)json.get("main")).get("temp_max").toString()));
+        dbEntity.setVisibility(Double.valueOf(json.get("visibility").toString()));
+        dbEntity.setWindSpeed(Double.valueOf(((JSONObject)json.get("wind")).get("speed").toString()));
+        dbEntity.setWindDegree(Double.valueOf(((JSONObject)json.get("wind")).get("deg").toString()));
+        dbEntity.setTime(Timestamp.from(Instant.ofEpochSecond(Long.valueOf(json.get("dt").toString()))));
+        dbEntity.setSunrise(Timestamp.from(Instant.ofEpochSecond(Long.valueOf(((JSONObject)json.get("sys")).get("sunrise").toString()))));
+        dbEntity.setSunset(Timestamp.from(Instant.ofEpochSecond(Long.valueOf(((JSONObject)json.get("sys")).get("sunset").toString()))));
+        dbEntity.setCityName(json.get("name").toString().toLowerCase());
+        return dbEntity;
     }
 
 }
