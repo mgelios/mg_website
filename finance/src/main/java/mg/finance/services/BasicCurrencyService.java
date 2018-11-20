4eba23c2f10ee40c55a7a6076d0affff574bfe25
mgelios <master_gelios@tut.by>
2018-11-20 17:52:39 +0300

    updated finance module service

--- finance/src/main/java/mg/finance/services/BasicCurrencyService.java
+++ finance/src/main/java/mg/finance/services/BasicCurrencyService.java
@@ -3,14 +3,19 @@ package mg.finance.services;
 import mg.finance.FinanceConfiguration;
 import mg.finance.dbentities.CurrencyDBEntity;
 import mg.finance.models.Currency;
+import mg.finance.models.CurrencyConversion;
 import mg.finance.models.CurrencyStatistics;
 import mg.finance.repositories.CurrencyConversionRepository;
 import mg.finance.repositories.CurrencyRepository;
 import mg.finance.repositories.CurrencyStatisticsRepository;
 import mg.finance.utils.CurrencyUrlBuilder;
 import mg.utils.JSONConsumer;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @Service
 public class BasicCurrencyService implements CurrencyService {
@@ -35,8 +40,23 @@ public class BasicCurrencyService implements CurrencyService {
 
     @Override
     public Currency getCurrencyValues() {
-        jsonConsumer.getJson(currencyUrlBuilder.buildCurrencyRateUrl("USD"));
+        JSONObject json = jsonConsumer.getJson(currencyUrlBuilder.buildCurrencyRateUrl("USD"));
         CurrencyDBEntity dbEntity = new CurrencyDBEntity();
+        dbEntity.setSystemId(json.getInt("Cur_ID"));
+        dbEntity.setDate(Timestamp.valueOf(json.getString("Date").replace("T", " ")));
+        dbEntity.setRate(json.getDouble("Cur_OfficialRate"));
+        dbEntity.setAbbreviation(json.getString("Cur_Abbreviation"));
+        dbEntity.setScale(json.getDouble("Cur_Scale"));
+        return null;
+    }
+
+    @Override
+    public List<CurrencyConversion> calculateCurrencyConversions() {
+        return null;
+    }
+
+    @Override
+    public List<CurrencyStatistics> getCurrencyStatistics() {
         return null;
     }
 }
--- finance/src/main/java/mg/finance/services/CurrencyService.java
+++ finance/src/main/java/mg/finance/services/CurrencyService.java
@@ -1,9 +1,16 @@
 package mg.finance.services;
 
 import mg.finance.models.Currency;
+import mg.finance.models.CurrencyConversion;
+import mg.finance.models.CurrencyStatistics;
+
+import java.util.List;
 
 public interface CurrencyService {
 
     Currency getCurrencyValues();
 
+    List<CurrencyConversion> calculateCurrencyConversions();
+
+    List<CurrencyStatistics> getCurrencyStatistics();
 }
