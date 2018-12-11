6299630d6e7e8ffb3793be9d563ccb4e78319fe7
mgelios <master_gelios@tut.by>
2018-12-11 17:28:15 +0300

    updated finance module, updated model

--- finance/src/main/java/mg/finance/models/CryptoMarket.java
+++ finance/src/main/java/mg/finance/models/CryptoMarket.java
@@ -1,6 +1,22 @@
 package mg.finance.models;
 
+import java.time.LocalDateTime;
+
 public class CryptoMarket {
 
     private Long id;
+
+    private Long totalUsd;
+    private Long totalUsdDayVolume;
+    private Long activeMarkets;
+    private Long activeCurrencies;
+    private double bitcoinPercent;
+    private LocalDateTime lastUpdated;
 }
+
+//    total_usd = models.IntegerField(default=0.0)
+//        total_usd_day_volume = models.IntegerField(default=0.0)
+//        active_markets = models.IntegerField(default=0.0)
+//        active_currencies = models.IntegerField(default=0)
+//        bitcoin_percent = models.FloatField(default=0.0)
+//        last_updated = models.DateTimeField(default=timezone.now)
