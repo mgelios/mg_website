d4a0c50e0dbae1a0bbea64af67d783b0bc521acf
mgelios <master_gelios@tut.by>
2018-12-12 14:27:10 +0300

    updated finance module, updated model

--- finance/src/main/java/mg/finance/models/CryptoMarket.java
+++ finance/src/main/java/mg/finance/models/CryptoMarket.java
@@ -12,6 +12,62 @@ public class CryptoMarket {
     private Long activeCurrencies;
     private double bitcoinPercent;
     private LocalDateTime lastUpdated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTotalUsd() {
+        return totalUsd;
+    }
+
+    public void setTotalUsd(Long totalUsd) {
+        this.totalUsd = totalUsd;
+    }
+
+    public Long getTotalUsdDayVolume() {
+        return totalUsdDayVolume;
+    }
+
+    public void setTotalUsdDayVolume(Long totalUsdDayVolume) {
+        this.totalUsdDayVolume = totalUsdDayVolume;
+    }
+
+    public Long getActiveMarkets() {
+        return activeMarkets;
+    }
+
+    public void setActiveMarkets(Long activeMarkets) {
+        this.activeMarkets = activeMarkets;
+    }
+
+    public Long getActiveCurrencies() {
+        return activeCurrencies;
+    }
+
+    public void setActiveCurrencies(Long activeCurrencies) {
+        this.activeCurrencies = activeCurrencies;
+    }
+
+    public double getBitcoinPercent() {
+        return bitcoinPercent;
+    }
+
+    public void setBitcoinPercent(double bitcoinPercent) {
+        this.bitcoinPercent = bitcoinPercent;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
 }
 
 //    total_usd = models.IntegerField(default=0.0)
