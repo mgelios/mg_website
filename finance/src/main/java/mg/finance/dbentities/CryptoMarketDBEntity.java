b2d277079398c402b078c1e65b9c69c2dd311c46
mgelios <master_gelios@tut.by>
2018-12-13 15:12:18 +0300

    updated finance module, updated model

--- finance/src/main/java/mg/finance/dbentities/CryptoMarketDBEntity.java
+++ finance/src/main/java/mg/finance/dbentities/CryptoMarketDBEntity.java
@@ -3,6 +3,7 @@ package mg.finance.dbentities;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 @Data
@@ -13,5 +14,18 @@ public class CryptoMarketDBEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "total_usd")
+    private Long totalUsd;
+    @Column(name = "total_usd_day_volume")
+    private Long totalUsdDayVolume;
+    @Column(name = "active_markets")
+    private Long activeMarkets;
+    @Column(name = "active_currencies")
+    private Long activeCurrencies;
+    @Column(name = "bitcoin_percent")
+    private double bitcoinPercent;
+    @Column(name = "last_updated")
+    private Timestamp lastUpdated;
 
 }
