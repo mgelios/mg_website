de45c2d4e94bdf230b3129ebd4282055bdee5528
mgelios <master_gelios@tut.by>
2019-03-26 16:59:16 +0300

    added header vue component, trying to pass props to component

--- webui/src/main/webapp/src/App.vue
+++ webui/src/main/webapp/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <Header headerText="sometext"/>
     <HelloWorld/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
+    Header,
     HelloWorld
   }
 }
