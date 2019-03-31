86c6fbfa2a89a8a45957b88e7ab19601d63c31a5
mgelios <master_gelios@tut.by>
2019-03-31 22:22:53 +0300

    started working on addid vuetify to webui

--- webui/src/main/webapp/src/components/HelloWorld.vue
+++ webui/src/main/webapp/src/components/HelloWorld.vue
@@ -1,55 +1,16 @@
 <template>
-    <div class="holder">
-      <h2>Hi</h2>
-      <form v-on:submit.prevent="addSkill">
-        <input type="text" placeholder="Enter you skill here" v-model="skill"/>
-      </form>
-      <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{index}}. {{data.skill}}</li>
-
-        <p v-if="skills.length >= 1">You have more than on skill</p>
-        <p v-else>You have one or less skills</p>
-      </ul>
-      <div>
-          {{weather}}
-      </div>
-    </div>
+  <v-toolbar app color="brown darken-4" dark>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Hello title</v-toolbar-title>
+  </v-toolbar>
 </template>
 
 <script>
-
-import axios from 'axios'
-
-export default {
-  name: 'Skills',
-  data: function(){
-    return {
-      skill: '',
-      skills: [
-          {"skill": "Vue.js"},
-          {"skill": "Spring boot"}
-      ],
-      weather: {}
-    }
-  },
-  methods: {
-    addSkill: function() {
-      this.skills.push({skill: this.skill});
-      this.skill = '';
-    }
-  },
-  mounted: function(){
-      axios
-        .get('http://localhost:8080/api/v1/weather/current')
-        .then(response => (this.weather = response));
-  },
-  props: {
-    btnState: true
+  export default {
+    name: 'HelloWorld'
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 
 </style>
