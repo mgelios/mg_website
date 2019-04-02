ce0a656e859ea44d0f1f33474011989a42a2a6dd
mgelios <master_gelios@tut.by>
2019-04-02 17:33:35 +0300

    switching to postgre started

--- finance/src/main/resources/sql/finance/finance_01.sql
+++ finance/src/main/resources/sql/finance/finance_01.sql
@@ -4,37 +4,34 @@ drop table if exists mg_currency;
 drop table if exists mg_crypto_currency;
 
 create table mg_currency (
-  id integer not null auto_increment,
+  id serial not null,
   system_id integer not null,
   date timestamp not null,
   abbreviation varchar(25) not null,
-  scale double not null,
+  scale real not null,
   name varchar(100) not null,
-  rate double not null,
+  rate real not null,
   primary key (id)
 );
 
 create table mg_currency_conversion (
-  id integer not null auto_increment,
-  currency_from integer not null,
-  currency_to integer not null,
-  value double not null,
+  id serial not null,
+  currency_from integer references mg_currency(id) not null,
+  currency_to integer references mg_currency(id) null,
+  value real not null,
   primary key (id),
-  constraint foreign key (currency_from) references mg_currency (id),
-  constraint foreign key (currency_to) references mg_currency (id),
   constraint unique_conversion unique (currency_to, currency_from)
 );
 
 create table mg_currency_statistics (
-  id integer not null auto_increment,
+  id serial not null,
   date timestamp not null,
-  currency integer not null,
-  rate double not null,
-  primary key (id),
-  constraint foreign key (currency) references mg_currency (id)
+  currency integer references mg_currency(id) null,
+  rate real not null,
+  primary key (id)
 );
 
 create table mg_crypto_currency(
-  id integer not null auto_increment
+  id integer not null
 );
 
