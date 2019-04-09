f656899e2aedb9238266edeae7be8aa99fad30c2
mgelios <master_gelios@tut.by>
2019-04-09 23:22:17 +0300

    trying to add yaml externalized configurations to project, added yaml to weather module

--- weather/src/main/java/mg/weather/WeatherConfiguration.java
+++ weather/src/main/java/mg/weather/WeatherConfiguration.java
@@ -2,15 +2,20 @@ package mg.weather;
 
 import lombok.Data;
 import mg.utils.JSONConsumer;
+import mg.utils.properties.YamlPropertyLoaderFactory;
 import mg.weather.task.WeatherSyncTask;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.YamlMapFactoryBean;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:/weather.properties")
+//@PropertySource("classpath:/weather.properties")
+@PropertySource(value = "classpath:/weather.yml", factory = YamlPropertyLoaderFactory.class)
 @ConfigurationProperties(prefix = "mg.weather")
 @Data
 public class WeatherConfiguration{
