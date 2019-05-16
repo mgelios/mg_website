d24d5672c75322ec4d2c2be6bca77c95de69608c
mgelios <master_gelios@tut.by>
2019-05-16 23:52:20 +0300

    added login page to routes list

--- webui/src/main/webapp/src/main.js
+++ webui/src/main/webapp/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import './filters/filters'
 import VueRouter from 'vue-router'
 import Dashboard from './components/dashboard/Dashboard'
+import Login from './components/profile/Login'
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
@@ -12,7 +13,8 @@ Vue.use(VueRouter);
 //const CryptoExample = CryptoCurrenciesTable;
 
 const routes = [
-    { path: '/dashboard', component: Dashboard }
+    { path: '/dashboard', component: Dashboard },
+    { path: '/login', component: Login}
 ];
 
 const router = new VueRouter({
