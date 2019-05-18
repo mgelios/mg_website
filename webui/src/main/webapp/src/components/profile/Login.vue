60f4b2ca47808dd777876e48a6c885ddffd2b36e
mgelios <master_gelios@tut.by>
2019-05-18 23:27:30 +0300

    updated login component styling to card element

--- webui/src/main/webapp/src/components/profile/Login.vue
+++ webui/src/main/webapp/src/components/profile/Login.vue
@@ -1,14 +1,21 @@
 <template>
-    <v-form>
-        <v-text-field
-                label="Login"
-                required
-                :counter="50"></v-text-field>
-        <v-text-field
-                label="Password"
-                required
-                :counter="50"></v-text-field>
-    </v-form>
+    <v-card>
+        <v-card-title>
+            <v-layout display-1>Login</v-layout>
+        </v-card-title>
+        <v-card-text>
+            <v-form>
+                <v-text-field
+                        label="Login"
+                        required
+                        :counter="50"></v-text-field>
+                <v-text-field
+                        label="Password"
+                        required
+                        :counter="50"></v-text-field>
+            </v-form>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
