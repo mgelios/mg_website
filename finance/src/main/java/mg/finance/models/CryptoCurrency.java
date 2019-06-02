a9a5a6d4cfeafb0db5e7a5d2c6884966f642da86
mgelios <master_gelios@tut.by>
2019-06-02 13:41:23 +0300

    updated crypto market models with lombok annotations

--- finance/src/main/java/mg/finance/models/CryptoCurrency.java
+++ finance/src/main/java/mg/finance/models/CryptoCurrency.java
@@ -1,7 +1,10 @@
 package mg.finance.models;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class CryptoCurrency {
 
     private Long id;
@@ -19,124 +22,4 @@ public class CryptoCurrency {
     private double percentChangeIn24h;
     private double percentChangeIn7d;
     private LocalDateTime lastUpdated;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-    public long getRank() {
-        return rank;
-    }
-
-    public void setRank(long rank) {
-        this.rank = rank;
-    }
-
-    public double getPriceUSD() {
-        return priceUSD;
-    }
-
-    public void setPriceUSD(double priceUSD) {
-        this.priceUSD = priceUSD;
-    }
-
-    public double getPriceBTC() {
-        return priceBTC;
-    }
-
-    public void setPriceBTC(double priceBTC) {
-        this.priceBTC = priceBTC;
-    }
-
-    public double getVolumeUSD24h() {
-        return volumeUSD24h;
-    }
-
-    public void setVolumeUSD24h(double volumeUSD24h) {
-        this.volumeUSD24h = volumeUSD24h;
-    }
-
-    public double getMarketCapUSD() {
-        return marketCapUSD;
-    }
-
-    public void setMarketCapUSD(double marketCapUSD) {
-        this.marketCapUSD = marketCapUSD;
-    }
-
-    public double getAvailableSupply() {
-        return availableSupply;
-    }
-
-    public void setAvailableSupply(double availableSupply) {
-        this.availableSupply = availableSupply;
-    }
-
-    public double getTotalSupply() {
-        return totalSupply;
-    }
-
-    public void setTotalSupply(double totalSupply) {
-        this.totalSupply = totalSupply;
-    }
-
-    public double getMaxSupply() {
-        return maxSupply;
-    }
-
-    public void setMaxSupply(double maxSupply) {
-        this.maxSupply = maxSupply;
-    }
-
-    public double getPercentChangeIn1h() {
-        return percentChangeIn1h;
-    }
-
-    public void setPercentChangeIn1h(double percentChangeIn1h) {
-        this.percentChangeIn1h = percentChangeIn1h;
-    }
-
-    public double getPercentChangeIn24h() {
-        return percentChangeIn24h;
-    }
-
-    public void setPercentChangeIn24h(double percentChangeIn24h) {
-        this.percentChangeIn24h = percentChangeIn24h;
-    }
-
-    public double getPercentChangeIn7d() {
-        return percentChangeIn7d;
-    }
-
-    public void setPercentChangeIn7d(double percentChangeIn7d) {
-        this.percentChangeIn7d = percentChangeIn7d;
-    }
-
-    public LocalDateTime getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(LocalDateTime lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
 }
--- finance/src/main/java/mg/finance/models/CryptoMarket.java
+++ finance/src/main/java/mg/finance/models/CryptoMarket.java
@@ -1,71 +1,17 @@
 package mg.finance.models;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class CryptoMarket {
 
     private Long id;
-
     private Long totalUsd;
     private Long totalUsdDayVolume;
     private Long activeMarkets;
     private Long activeCurrencies;
     private double bitcoinPercent;
     private LocalDateTime lastUpdated;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getTotalUsd() {
-        return totalUsd;
-    }
-
-    public void setTotalUsd(Long totalUsd) {
-        this.totalUsd = totalUsd;
-    }
-
-    public Long getTotalUsdDayVolume() {
-        return totalUsdDayVolume;
-    }
-
-    public void setTotalUsdDayVolume(Long totalUsdDayVolume) {
-        this.totalUsdDayVolume = totalUsdDayVolume;
-    }
-
-    public Long getActiveMarkets() {
-        return activeMarkets;
-    }
-
-    public void setActiveMarkets(Long activeMarkets) {
-        this.activeMarkets = activeMarkets;
-    }
-
-    public Long getActiveCurrencies() {
-        return activeCurrencies;
-    }
-
-    public void setActiveCurrencies(Long activeCurrencies) {
-        this.activeCurrencies = activeCurrencies;
-    }
-
-    public double getBitcoinPercent() {
-        return bitcoinPercent;
-    }
-
-    public void setBitcoinPercent(double bitcoinPercent) {
-        this.bitcoinPercent = bitcoinPercent;
-    }
-
-    public LocalDateTime getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(LocalDateTime lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
 }
