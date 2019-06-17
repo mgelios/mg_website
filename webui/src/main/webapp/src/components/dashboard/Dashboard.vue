4dfb6391694f131463ce20bea3aa6344526abbd6
mgelios <master_gelios@tut.by>
2019-06-17 17:13:08 +0300

    updated dashboard component styling

--- webui/src/main/webapp/src/components/dashboard/Dashboard.vue
+++ webui/src/main/webapp/src/components/dashboard/Dashboard.vue
@@ -1,9 +1,35 @@
 <template>
     <div>
-        <v-navigation-drawer app v-model="navDrawer">
+        <v-navigation-drawer app v-model="navDrawer" :mini-variant.sync="miniToolbar">
             <v-toolbar flat>
-                <v-icon x-large>pool</v-icon>
+                <v-list>
+                    <v-list-tile avatar>
+                        <v-list-tile-avatar>
+                            <v-icon x-large>dashboard</v-icon>
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title>Dashboard</v-list-tile-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn icon @click.stop="miniToolbar = !miniToolbar">
+                                <v-icon>chevron_left</v-icon>
+                            </v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list>
             </v-toolbar>
+            <v-list>
+                <v-list-tile
+                        v-for="navItem in navigationItems"
+                        :key="navItem.title">
+                    <v-list-tile-action>
+                        <v-icon>{{navItem.icon}}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{navItem.title}}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
         </v-navigation-drawer>
         <v-toolbar app flat>
             <v-toolbar-side-icon @click.native="navDrawer = !navDrawer"></v-toolbar-side-icon>
@@ -69,7 +95,26 @@
         },
         data: function() {
             return {
-                navDrawer: true
+                navDrawer: true,
+                miniToolbar: true,
+                navigationItems: [
+                    {
+                        title: 'Weather',
+                        icon: 'cloud',
+                        route: ''
+                    },
+                    {
+                        title: 'Finance',
+                        icon: 'euro_symbol',
+                        route: ''
+                    },
+                    {
+                        title: 'Crypto market',
+                        icon: 'list_alt',
+                        route: ''
+                    }
+
+                ]
             }
         }
     }
