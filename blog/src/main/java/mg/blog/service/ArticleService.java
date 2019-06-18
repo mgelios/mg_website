247ced26dd04ab6a4d461d30beeaa3c83268edab
mgelios <master_gelios@tut.by>
2019-06-18 16:55:59 +0300

    started implementing services of blog module

--- blog/src/main/java/mg/blog/service/ArticleService.java
+++ blog/src/main/java/mg/blog/service/ArticleService.java
@@ -1,4 +1,20 @@
 package mg.blog.service;
 
+import mg.blog.model.Article;
+import mg.blog.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public Article createArticle(String content) {
+        Article article = new Article();
+        SecurityContextHolder.getContext().getAuthentication().getName();
+        return null;
+    }
 }
