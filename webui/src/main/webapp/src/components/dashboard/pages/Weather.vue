2a1d6a593e3c48680e5b7fc8552e5ee9176710ab
mgelios <master_gelios@tut.by>
2019-06-21 20:01:33 +0300

    created weather page component

---
+++ webui/src/main/webapp/src/components/dashboard/pages/Weather.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-toolbar app color="brown darken-4" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Weather title</v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: 'Weather'
+    }
+</script>
+
+<style>
+
+</style>
