a317fd1eff6470af04be95bff5fa69fc88acc3fe
mgelios <master_gelios@tut.by>
2019-06-22 17:18:20 +0300

    created finance page component

---
+++ webui/src/main/webapp/src/components/dashboard/pages/Finance.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-toolbar app color="brown darken-4" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Finance title</v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: 'Finance'
+    }
+</script>
+
+<style>
+
+</style>
