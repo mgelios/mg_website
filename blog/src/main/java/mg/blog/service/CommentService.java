25f52d80238300f78ee13175ff3b5201fe7b2384
mgelios <master_gelios@tut.by>
2019-07-04 16:03:14 +0300

    small blog service update

--- blog/src/main/java/mg/blog/service/CommentService.java
+++ blog/src/main/java/mg/blog/service/CommentService.java
@@ -1,4 +1,18 @@
 package mg.blog.service;
 
+import mg.blog.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
+
+    private CommentRepository commentRepository;
+
+    public void createComment() {
+
+    }
+
+    public void updateComment() {
+
+    }
 }
