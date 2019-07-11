6414145bc062dd9e88140ddca2b68ec9348bc941
mgelios <master_gelios@tut.by>
2019-07-11 15:59:11 +0300

    updated current weather card with icon of current weather and additional info markup and styling

--- webui/src/main/webapp/src/components/dashboard/pages/cards/CurrentWeatherCard.vue
+++ webui/src/main/webapp/src/components/dashboard/pages/cards/CurrentWeatherCard.vue
@@ -4,10 +4,45 @@
             <v-layout headline class="white--text">{{currentWeather.cityName | capitalize}} {{currentWeather.temperature | degreeCelsius}}</v-layout>
         </v-card-title>
         <v-card-text>
-            <v-img v-bind:src="imageUrl"></v-img>
-            <v-layout display-1>{{currentWeather.description}}</v-layout>
-            <v-layout headline> {{currentWeather.minimalTemperature | degreeCelsius}} - {{currentWeather.maximumTemperature | degreeCelsius}} </v-layout>
-            <v-layout headline></v-layout>
+            <v-layout row>
+                <v-flex xs12 sm6>
+                    <v-img v-bind:src="getImage(currentWeather.icon)"></v-img>
+                </v-flex>
+                <v-flex xs12 sm6>
+                    <v-layout headline class="text-xs-center">{{currentWeather.description | capitalize}}</v-layout>
+                    <v-list>
+                        <v-list-tile>
+                            <v-list-tile-content>Humidity:</v-list-tile-content>
+                            <v-list-tile-action>{{currentWeather.humidity}}%</v-list-tile-action>
+                        </v-list-tile>
+                        <v-divider class="ma-0"></v-divider>
+                        <v-list-tile>
+                            <v-list-tile-content>Pressure:</v-list-tile-content>
+                            <v-list-tile-action>{{(currentWeather.pressure*(0.750062)).toFixed(2)}}</v-list-tile-action>
+                        </v-list-tile>
+                        <v-divider class="ma-0"></v-divider>
+                        <v-list-tile>
+                            <v-list-tile-content>Visibility:</v-list-tile-content>
+                            <v-list-tile-action>{{currentWeather.visibility}} m</v-list-tile-action>
+                        </v-list-tile>
+                        <v-divider class="ma-0"></v-divider>
+                        <v-list-tile>
+                            <v-list-tile-content>Wind speed:</v-list-tile-content>
+                            <v-list-tile-action>{{currentWeather.windSpeed}} m/s</v-list-tile-action>
+                        </v-list-tile>
+                        <v-divider class="ma-0"></v-divider>
+                        <v-list-tile>
+                            <v-list-tile-content>Sunrise:</v-list-tile-content>
+                            <v-list-tile-action>{{currentWeather.sunrise[3]+':'+currentWeather.sunrise[4]}}</v-list-tile-action>
+                        </v-list-tile>
+                        <v-divider class="ma-0"></v-divider>
+                        <v-list-tile>
+                            <v-list-tile-content>Sunset:</v-list-tile-content>
+                            <v-list-tile-action>{{currentWeather.sunset[3]+':'+currentWeather.sunset[4]}}</v-list-tile-action>
+                        </v-list-tile>
+                    </v-list>
+                </v-flex>
+            </v-layout>
         </v-card-text>
     </v-card>
 </template>
@@ -19,6 +54,8 @@
         name: 'CurrentWeatherCard',
         data: function() {
             return {
+                weatherImagesFolder: '../../../../assets/img/weather/',
+                weatherImagesExtension: '.svg'
             }
         },
         mounted: function() {
@@ -27,13 +64,12 @@
         computed: {
             ...mapState({
                 currentWeather : state => state.Weather.currentWeather
-            }),
-            imageUrl: function() {
-//                if (this.currentWeather.icon) {
-//                    return '../../../assets/img/weather/' + this.currentWeather.icon + '.svg';
-//                } else {
-                    return '../../../assets/img/weather/01d.svg';
-//                }
+            })
+        },
+        methods: {
+            getImage: function(name) {
+                var images = require.context('../../../../assets/img/weather/', false, /\.svg$/);
+                return images('./' + name + '.svg');
             }
         }
     }
