465df4a48e41ec01a12b303015a52bcdbc78163e
mgelios <antonnadezhkin@gmail.com>
2020-10-03 18:17:42 +0300

    removed unnecessary @Column annotations from weather entities (got info about hibernate naming strategies and default spring naming strategy)

--- weather/src/main/java/mg/weather/entity/CurrentWeather.java
+++ weather/src/main/java/mg/weather/entity/CurrentWeather.java
@@ -1,7 +1,6 @@
 package mg.weather.entity;
 
 import lombok.Data;
-import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
 
 import javax.persistence.*;
@@ -17,53 +16,23 @@ public class CurrentWeather {
 
     @Id
     @GeneratedValue
-    @Column(name = "uuid")
     private UUID uuid;
-
-    @Column(name = "time")
     private Timestamp time;
-
-    @Field
-    @Column(name = "city_name")
     private String cityName;
-
-    @Column(name = "longitude")
     private double longitude;
-    @Column(name = "latitude")
     private double latitude;
-
-    @Column(name = "main_info")
     private String mainInfo;
-    @Field
-    @Column(name = "description")
     private String description;
-    @Column(name = "icon")
     private String icon;
-
-    @Column(name = "temperature")
     private double temperature;
-    @Column(name = "pressure")
     private double pressure;
-    @Column(name = "humidity")
     private double humidity;
-    @Column(name = "min_temp")
     private double minimalTemperature;
-    @Column(name = "max_temp")
     private double maximumTemperature;
-    @Column(name = "visibility")
     private double visibility;
-
-    @Column(name = "wind_speed")
     private double windSpeed;
-    @Column(name = "wind_degree")
     private double windDegree;
-
-    @Column(name = "sunrise")
     private Timestamp sunrise;
-    @Column(name = "sunset")
     private Timestamp sunset;
-
-    @Column(name = "uvi")
     private double uvi;
-
 }
--- weather/src/main/java/mg/weather/entity/WeatherForecast.java
+++ weather/src/main/java/mg/weather/entity/WeatherForecast.java
@@ -13,43 +13,22 @@ public class WeatherForecast {
 
     @Id
     @GeneratedValue
-    @Column(name = "uuid")
     private UUID uuid;
-
-    @Column(name = "time")
     private Timestamp time;
-
-    @Column(name = "temperature")
     private double temperature;
-    @Column(name = "min_temp")
+    //TODO: rename field or column in liquibase script
     private double minimalTemperature;
-    @Column(name = "max_temp")
     private double maximumTemperature;
-    @Column(name = "pressure")
     private double pressure;
-    @Column(name = "sea_level")
     private double seaLevel;
-    @Column(name = "ground_level")
     private double groundLevel;
-    @Column(name = "humidity")
     private double humidity;
-
-    @Column(name = "main_info")
     private String mainInfo;
-    @Column(name = "description")
     private String description;
-    @Column(name = "icon")
     private String icon;
-
-    @Column(name = "wind_speed")
     private double windSpeed;
-    @Column(name = "wind_degree")
     private double windDegree;
-
-    @Column(name = "city_name")
     private String cityName;
-
-    @Column(name = "update_time")
     private Timestamp updateTime;
 
 }
