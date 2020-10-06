3ade01bad0e096b0d0a07f02a817a60dc0c0828b
mgelios <antonnadezhkin@gmail.com>
2020-10-06 10:37:37 +0300

    removed unnecessary @Column annotations from currency entities

--- finance/src/main/java/mg/finance/entity/CryptoMarket.java
+++ finance/src/main/java/mg/finance/entity/CryptoMarket.java
@@ -12,19 +12,11 @@ public class CryptoMarket {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
-
-    @Column(name = "total_usd")
     private Long totalUsd;
-    @Column(name = "total_usd_day_volume")
     private Long totalUsdDayVolume;
-    @Column(name = "active_markets")
     private Long activeMarkets;
-    @Column(name = "active_currencies")
     private Long activeCurrencies;
-    @Column(name = "bitcoin_percent")
     private double bitcoinPercent;
-    @Column(name = "last_updated")
     private Timestamp lastUpdated;
 }
