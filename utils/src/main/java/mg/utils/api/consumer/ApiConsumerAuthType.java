2119b0ca3146b78da4a4aacd693495f74b5fb201
mgelios <antonnadezhkin@gmail.com>
2020-10-18 14:37:11 +0300

    update for api consumer utils

---
+++ utils/src/main/java/mg/utils/api/consumer/ApiConsumerAuthType.java
@@ -0,0 +1,8 @@
+package mg.utils.api.consumer;
+
+public enum ApiConsumerAuthType {
+
+    API_KEY,
+    TOKEN,
+    CLIENT_ID_AND_SECRET
+}
--- utils/src/main/java/mg/utils/api/consumer/ApiConsumerService.java
+++ utils/src/main/java/mg/utils/api/consumer/ApiConsumerService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ApiConsumerService {
 
-    public void performApiCall() {
+    public void performApiCall(ApiConsumerAuthType authType) {
 
     }
 }
