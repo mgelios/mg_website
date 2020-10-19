20bcc93a907428c7dfceec5770e3dc41f4a47170
mgelios <antonnadezhkin@gmail.com>
2020-10-19 11:14:51 +0300

    update for api consumer utils

--- utils/src/main/java/mg/utils/api/consumer/ApiConsumerRepository.java
+++ utils/src/main/java/mg/utils/api/consumer/ApiConsumerRepository.java
@@ -3,8 +3,11 @@ package mg.utils.api.consumer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ApiConsumerRepository extends CrudRepository<ApiConsumer, UUID> {
+
+    Optional<ApiConsumer> findByName(String name);
 }
--- utils/src/main/java/mg/utils/api/consumer/ApiConsumerService.java
+++ utils/src/main/java/mg/utils/api/consumer/ApiConsumerService.java
@@ -1,11 +1,48 @@
 package mg.utils.api.consumer;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Slf4j
 @Service
+@AllArgsConstructor
 public class ApiConsumerService {
 
-    public void performApiCall(ApiConsumerAuthType authType) {
+    private final ApiConsumerRepository apiConsumerRepository;
+
+    public void performApiCall(String name, ApiConsumerAuthType authType) {
+        Optional<ApiConsumer> optionalOfConsumer = apiConsumerRepository.findByName(name);
+        if (!optionalOfConsumer.isPresent()) {
+            log.error("No api consumer with name {} present", name);
+            return;
+        }
+        ApiConsumer consumer = optionalOfConsumer.get();
+
+        switch (authType) {
+            case API_KEY:
+                performApiCallWithApiKey(consumer.getApiKey());
+                break;
+            case TOKEN:
+                performApiCallWithToken(consumer.getToken());
+                break;
+            case CLIENT_ID_AND_SECRET:
+                perfromApiCallWithClientIdAndSecret(consumer.getClientId(), consumer.getClientSecret());
+                break;
+        }
+    }
+
+    private void performApiCallWithApiKey(String apiKey) {
+
+    }
+
+    private void performApiCallWithToken(String token) {
+
+    }
+
+    private void perfromApiCallWithClientIdAndSecret(String clientId, String clientSecret) {
 
     }
 }
