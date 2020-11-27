304f870c92f030f0bc5ccfed85b3b748eae5de4c
mgelios <antonnadezhkin@gmail.com>
2020-11-27 10:34:44 +0300

    made back-end session police stateless

--- main/src/main/java/mg/security/WebSecurityConfig.java
+++ main/src/main/java/mg/security/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
@@ -69,9 +70,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                addFilterBefore(basicCORSFilter, LogoutFilter.class).
-                authorizeRequests()
+        http
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(basicCORSFilter, LogoutFilter.class)
+                .authorizeRequests()
                     .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                     .antMatchers(AUTH_ALL).permitAll()
                     .antMatchers(AUTH_AUTHENTICATED).authenticated()
