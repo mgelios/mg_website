94ff0aed3a69a173fd2f1c746b4c3311c01ef8db
mgelios <antonnadezhkin@gmail.com>
2020-12-07 10:54:45 +0300

    updating api consumer module

--- utils/src/main/java/mg/utils/api/consumer/ApiConsumerDto.java
+++ utils/src/main/java/mg/utils/api/consumer/ApiConsumerDto.java
@@ -1,4 +1,18 @@
 package mg.utils.api.consumer;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class ApiConsumerDto {
+
+    private UUID uuid;
+    private String name;
+    private String apiKey;
+    private String token;
+    private String clientId;
+    private String clientSecret;
 }
--- utils/src/main/java/mg/utils/api/consumer/ApiConsumerMapper.java
+++ utils/src/main/java/mg/utils/api/consumer/ApiConsumerMapper.java
@@ -1,7 +1,14 @@
 package mg.utils.api.consumer;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
-public interface ApiConsumerMapper {
+public abstract class ApiConsumerMapper {
+
+    public static final ApiConsumerMapper INSTANCE = Mappers.getMapper(ApiConsumerMapper.class);
+
+    public abstract ApiConsumerDto mapToDto(ApiConsumer entity);
+
+    public abstract ApiConsumer mapToEntity(ApiConsumerDto dto);
 }
