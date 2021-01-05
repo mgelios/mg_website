7f8957943e19be2a3bad0592431faaf63a631463
mgelios <antonnadezhkin@gmail.com>
2021-01-05 11:05:13 +0300

    updating currency state management on ui

--- webui/src/main/webapp/src/store/currency/actions.js
+++ webui/src/main/webapp/src/store/currency/actions.js
@@ -1,3 +1,17 @@
 export const actions = Object.freeze({
-
+    fetchExchangeRates,
+    fetchCurrencyConversions,
+    fetchCurrencyStatistics,
 });
+
+async function fetchExchangeRates({ commit }) {
+
+}
+
+async function fetchCurrencyConversions({ commit }) {
+
+}
+
+async function fetchCurrencyStatistics({ commit }) {
+
+}
