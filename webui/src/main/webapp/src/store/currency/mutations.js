7f3b7e1ec31628eabdd59fa8380e82112fe181dd
mgelios <antonnadezhkin@gmail.com>
2021-01-07 12:27:11 +0300

    updating currency state management on ui

--- webui/src/main/webapp/src/store/currency/mutations.js
+++ webui/src/main/webapp/src/store/currency/mutations.js
@@ -1,3 +1,10 @@
-export const mutations = Object.freeze({
+import {types} from "./types";
 
+export const mutations = Object.freeze({
+    [types.SET_EXCHANGE_RATES](state, values) {
+
+    },
+    [types.SET_CURRENCY_CONVERSIONS](state, values) {
+
+    }
 });
