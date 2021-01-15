62d5a6f059c231d2136f6039a8cf5f6803ef1981
mgelios <antonnadezhkin@gmail.com>
2021-01-15 10:33:07 +0300

    updating state management on ui

---
+++ webui/src/main/webapp/src/models/weather/currentWeatherModel.js
@@ -0,0 +1,50 @@
+export class CurrentWeatherModel {
+
+    /**
+     * Current Weather config
+     *
+     * @param {Object} config
+     * @param {String} config.uuid
+     * @param {String} config.cityName
+     * @param {String} config.mainInfo
+     * @param {String} config.description
+     * @param {String} config.icon
+     * @param {Number} config.longitude
+     * @param {Number} config.latitude
+     * @param {Number} config.temperature
+     * @param {Number} config.pressure
+     * @param {Number} config.humidity
+     * @param {Number} config.minimalTemperature
+     * @param {Number} config.maximumTemperature
+     * @param {Number} config.visibility
+     * @param {Number} config.windSpeed
+     * @param {Number} config.windDegree
+     * @param {Number} config.uvi
+     * @param {String} config.time
+     * @param {String} config.sunrise
+     * @param {String} config.sunset
+     *
+     */
+    constructor(config) {
+        config = config || {};
+        this.uuid = config.uuid;
+        this.cityName = config.cityName;
+        this.mainInfo = config.mainInfo;
+        this.description = config.description;
+        this.icon = config.icon;
+        this.longitude = config.longitude;
+        this.latitude = config.latitude;
+        this.temperature = config.temperature;
+        this.pressure = config.pressure;
+        this.humidity = config.humidity;
+        this.minimalTemperature = config.minimalTemperature;
+        this.maximumTemperature = config.maximumTemperature;
+        this.visibility = config.visibility;
+        this.windSpeed = config.windSpeed;
+        this.windDegree = config.windDegree;
+        this.uvi = config.uvi;
+        this.time = config.time;
+        this.sunrise = config.sunrise;
+        this.sunset = config.sunset;
+    }
+}
