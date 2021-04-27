15d83a55a016686f2ae2a22943ff9245ee4d7861
mgelios <antonnadezhkin@gmail.com>
2021-04-27 21:52:49 +0300

    added skeleton of the radiot info pages

---
+++ webui/src/main/webapp/src/components/info/radiot/RadiotPodcastsInfo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        {{articles}}
+    </div>
+</template>
+<script>
+    import { mapState, mapActions } from 'vuex';
+
+    export default {
+        name: 'RadiotPodcastsInfo',
+        components: {
+        },
+        data: function(){
+            return {
+
+            }
+        },
+        computed: {
+            ...mapState('radio-t/podcast', ['podcasts']),
+        },
+        mounted() {
+            this.fetchArticles();
+        },
+        methods: {
+            ...mapActions('radio-t/podcast', ['fetchPodcasts']),
+
+        }
+    }
+</script>
