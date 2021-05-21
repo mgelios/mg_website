3801a89315eda0212ab52ebc7fa1ab62b3f12fbf
mgelios <antonnadezhkin@gmail.com>
2021-05-21 10:08:36 +0300

    added getters for currency state module

--- webui/src/main/webapp/src/store/currency/getters.js
+++ webui/src/main/webapp/src/store/currency/getters.js
@@ -1,3 +1,12 @@
 export const getters = Object.freeze({
-
+    statisticsUsd,
+    statisticsEur,
 });
+
+function statisticsUsd(state) {
+    return state.get('USD');
+}
+
+function statisticsEur(state) {
+    return state.get('EUR');
+}
