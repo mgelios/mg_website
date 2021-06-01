73823aa2d31e453d38f0a3a5e31a21df589580d0
mgelios <antonnadezhkin@gmail.com>
2021-06-01 11:09:15 +0300

    added mutations for monthly currency statistics as subset of year statistics

--- webui/src/main/webapp/src/store/currency/mutations.js
+++ webui/src/main/webapp/src/store/currency/mutations.js
@@ -8,15 +8,31 @@ export const mutations = Object.freeze({
         state.currencyConversions = values;
     },
     [types.SET_ANNUAL_CURRENCY_STATISTICS_EUR](state, values) {
-        state.currencyStatisticsEur = values;
-        state.currencyStatisticsEur.sort((a, b) => {
+        state.annualCurrencyStatisticsEur = values;
+        state.annualCurrencyStatisticsEur.sort((a, b) => {
             return a.date.value - b.date.value;
         });
     },
     [types.SET_ANNUAL_CURRENCY_STATISTICS_USD](state, values) {
-        state.currencyStatisticsUsd = values;
-        state.currencyStatisticsUsd.sort((a, b) => {
+        state.annualCurrencyStatisticsUsd = values;
+        state.annualCurrencyStatisticsUsd.sort((a, b) => {
             return a.date.value - b.date.value;
         });
+    },
+    [types.SET_MONTHLY_CURRENCY_STATISTICS_EUR](state, values) {
+        state.monthlyCurrencyStatisticsEur = values;
+        state.monthlyCurrencyStatisticsEur.sort((a, b) => {
+            return a.date.value - b.date.value;
+        });
+        state.monthlyCurrencyStatisticsEur = state.monthlyCurrencyStatisticsEur
+            .slice(state.monthlyCurrencyStatisticsEur.length - 31, state.monthlyCurrencyStatisticsEur.length);
+    },
+    [types.SET_MONTHLY_CURRENCY_STATISTICS_USD](state, values) {
+        state.monthlyCurrencyStatisticsUsd = values;
+        state.monthlyCurrencyStatisticsUsd.sort((a, b) => {
+            return a.date.value - b.date.value;
+        });
+        state.monthlyCurrencyStatisticsUsd = state.monthlyCurrencyStatisticsUsd
+            .slice(state.monthlyCurrencyStatisticsUsd.length - 31, state.monthlyCurrencyStatisticsUsd.length);
     }
 });
