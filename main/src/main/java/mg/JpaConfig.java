4086955b22c9c1d5aa7e7ff534dadf307ed08cf7
mgelios <antonnadezhkin@gmail.com>
2023-04-29 21:17:27 +0200

    removed old CORS filter, updated JpaConfig, possibly there will be updates

--- main/src/main/java/mg/JpaConfig.java
+++ main/src/main/java/mg/JpaConfig.java
@@ -1,12 +1,11 @@
 package mg;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
-import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -17,7 +16,7 @@ import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,7 +29,7 @@ public class JpaConfig {
 
     protected Map<String, Object> jpaProperties() {
         Map<String, Object> props = new HashMap<>();
-        props.put("hibernate.physical_naming_strategy", SpringPhysicalNamingStrategy.class.getName());
+        props.put("hibernate.physical_naming_strategy", CamelCaseToUnderscoresNamingStrategy.class.getName());
         props.put("hibernate.implicit_naming_strategy", SpringImplicitNamingStrategy.class.getName());
         return props;
     }
@@ -46,11 +45,11 @@ public class JpaConfig {
                 .build();
     }
 
-    @Primary
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManager) {
-        return new JpaTransactionManager(entityManager);
-    }
+//    @Primary
+//    @Bean
+//    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManager) {
+//        return new JpaTransactionManager(entityManager);
+//    }
 
     @Bean
     @ConditionalOnProperty(prefix = "spring.jpa", name = "open-in-view", havingValue = "true", matchIfMissing = true)
