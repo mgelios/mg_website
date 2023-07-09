1f314924fa4db1572432687fec238887a1abbd42
mgelios <antonnadezhkin@gmail.com>
2023-07-09 16:00:15 +0200

    resolved conflict between ui and grafana ports

--- weather/src/test/java/mg/weather/BasicSpringContextTest.java
+++ weather/src/test/java/mg/weather/BasicSpringContextTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ContextConfiguration(classes = WeatherTestConfig.class)
 @ExtendWith(SpringExtension.class)
-public class BasicSpringBootTest {
+@TestPropertySource(locations = "/classpath:weather-test-config.properties")
+public class BasicSpringContextTest {
 
     @Autowired
     private CurrentWeatherRepository currentWeatherRepository;
