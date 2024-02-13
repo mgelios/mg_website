4c0541f991420f9da71ec653322b3456dc73f7c4
mgelios <antonnadezhkin@gmail.com>
2024-02-13 11:10:34 +0100

    added few classes to external api cache module

---
+++ external-api-cache/src/main/java/mg/api/external/cache/controller/ExternalApiCacheController.java
@@ -0,0 +1,19 @@
+package mg.api.external.cache.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/v1/external-api-cache")
+@AllArgsConstructor
+public class ExternalApiCacheController {
+
+    @GetMapping("/{}")
+    public String getCacheValues() {
+        return null;
+    }
+}
---
+++ external-api-cache/src/main/java/mg/api/external/cache/repository/ExternalApiCacheRepository.java
@@ -0,0 +1,8 @@
+package mg.api.external.cache.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface ExternalApiCacheRepository extends CrudRepository<, UUID> {
+}
