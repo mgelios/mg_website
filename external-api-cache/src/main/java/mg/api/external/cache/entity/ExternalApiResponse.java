2a348f12b52bded171204527d153c83f57d38d9f
mgelios <antonnadezhkin@gmail.com>
2024-02-19 12:41:06 +0100

    adding classes to cache module

---
+++ external-api-cache/src/main/java/mg/api/external/cache/entity/ExternalApiResponse.java
@@ -0,0 +1,22 @@
+package mg.api.external.cache.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class ExternalApiResponse {
+
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+    private String body;
+    private OffsetDateTime dateTime;
+}
--- external-api-cache/src/main/java/mg/api/external/cache/repository/ExternalApiCacheRepository.java
+++ external-api-cache/src/main/java/mg/api/external/cache/repository/ExternalApiCacheRepository.java
@@ -1,8 +1,9 @@
 package mg.api.external.cache.repository;
 
+import mg.api.external.cache.entity.ExternalApiResponse;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface ExternalApiCacheRepository extends CrudRepository<, UUID> {
+public interface ExternalApiCacheRepository extends CrudRepository<ExternalApiResponse, UUID> {
 }
